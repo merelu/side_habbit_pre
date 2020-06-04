7724def0a574107ff909cfa4c551d32a71904277
merelu <gyuha0310@gmail.com>
2020-06-04 21:55:04 +0900

    20200604 calendar 수정

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,187 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Calendar: React.FC = () => {
-    //id생성하기위함 
+    let [_date, setDate] = useState<Date>(new Date());
+    const month: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    let generatorCalendar = () => {
+        console.log(_date.getMonth());
+        const firstdayName: number = new Date(_date.getFullYear(), _date.getMonth(), 1).getDay();
+
+        const lastday: number = new Date(_date.getFullYear(), _date.getMonth() + 1, 0).getDate();
+
+        const preLastday: number = new Date(_date.getFullYear(), _date.getMonth(), 0).getDate();
+
+        let startDayCount: number = 1;
+        let lastDayCount: number = 1;
+        let calendarList = [];
+
+        for (let i = 0; i < 6; i++) {
+            for (let j = 0; j < 7; j++) {
+                if (i === 0 && j < firstdayName) {
+                    calendarList.push(
+                        <div className="dayBox">
+                            <span className="text">{preLastday - (firstdayName - 1) + j}</span>
+                            <span id={_date.getFullYear().toString()+setFixNum(_date.getMonth()) + setFixNum(preLastday - (firstdayName - 1) + j)}></span>
+                        </div>
+                    )
+                } else if (i === 0 && j === firstdayName) {
+                    calendarList.push(
+                        <div className="dayBox">
+                            <span className="text">{startDayCount++}</span>
+                            <span id={_date.getFullYear().toString()+setFixNum(_date.getMonth()+1) + setFixNum(startDayCount)}></span>
+                        </div>
+                    )
+                } else if (i === 0 && j > firstdayName) {
+                    calendarList.push(
+                        <div className="dayBox">
+                            <span className="text">{startDayCount++}</span>
+                            <span id={_date.getFullYear().toString()+setFixNum(_date.getMonth()+1) + setFixNum(startDayCount)}></span>
+                        </div>
+                    )
+                } else if (i > 0 && startDayCount <= lastday) {
+                    calendarList.push(
+                        <div className="dayBox">
+                            <span className="text">{startDayCount++}</span>
+                            <span id={_date.getFullYear().toString()+setFixNum(_date.getMonth()+1) + setFixNum(startDayCount)}></span>
+                        </div>
+                    )
+                } else if (startDayCount > lastday) {
+                    calendarList.push(
+                        <div className="dayBox">
+                            <span className="text">{lastDayCount++}</span>
+                            <span id={_date.getFullYear().toString()+setFixNum(_date.getMonth()+2) + setFixNum(lastDayCount)}></span>
+                        </div>
+                    )
+                }
+            }
+        }
+        return calendarList;
+    }
+    const setFixNum = (num: number): string => {
+        let fixNum: string = "";
+        if (num < 10) {
+            fixNum = "0" + num.toString();
+        } else {
+            fixNum = num.toString();
+        }
+        return fixNum;
+    }
     return (
         <div className="callender">
             <div className="month">
                 <ul>
-                    <li className="prev">&#10094;</li>
-                    <li className="next">&#10095;</li>
+                    <li className="prev" onClick={function () { setDate(new Date(_date.getFullYear(), _date.getMonth() - 1)) }}>&#10094;</li>
+                    <li className="next" onClick={function () { setDate(new Date(_date.getFullYear(), _date.getMonth() + 1)) }}>&#10095;</li>
                     <li>
-                        June<br />
-                        <span >2020</span>
+                        <span>{month[_date.getMonth()]}</span>
+                        <br />
+                        <span>{_date.getFullYear()}</span>
                     </li>
                 </ul>
             </div>
 
-            <div className="days">
-                
+            <div className="calenderBody">
+                <div className="dayBox">
+                    <span className="text">Sun</span>
+                </div>
+                <div className="dayBox">
+                    <span className="text">Mon</span>
+                </div>
+                <div className="dayBox">
+                    <span className="text">Tue</span>
+                </div>
+                <div className="dayBox">
+                    <span className="text">Wed</span>
+                </div>
+                <div className="dayBox">
+                    <span className="text">Thr</span>
+                </div>
+                <div className="dayBox">
+                    <span className="text">Fri</span>
+                </div>
+                <div className="dayBox">
+                    <span className="text">Sat</span>
+                </div>
+
+                {generatorCalendar()}
             </div>
         </div>
     );
 }
-/*
-const SetCalendarDate = () => {
-    let calHtml: string = "";
-    const today: Date = new Date();
-    const dayOfWeek: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-    const setDate: Date = new Date(today.getFullYear(), today.getMonth() - 1, 1);
-
-    //getDay weekday (0-6)
-    const firstDayName: number = setDate.getDay();
-
-    const lastday: number = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
-
-    //preMonthLastDay
-    const preLastDay: number = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
-
-    //이번달 날짜 개수 세기 위한 변수
-    let startDayCount: number = 1;
-    let lastDayCount: number = 1;
-
-    //week
-    for (let i: number = 0; i < 7; i++) {
-        //dayname
-        for (let j: number = 0; j < 7; j++) {
-            if (i = 0) {
-                calHtml += `
-                <div class='callendar_day_name horizonMargin>
-                <span>${dayOfWeek[j]}</span>
-                </div>`
-            }
-            //1주차 이번달 시작요일 이전일때
-            else if (i == 1 && j < firstDayName) {
-                if (j == 0) {
-                    calHtml += `
-                    <div class='calendar_day horizonMargin verticleMargin'>
-                    <span>${(preLastDay - (firstDayName - 1) + j)}</span><span></span>
-                    </div>`;
-                } else if (j == 6) {
-                    calHtml += `
-                    <div class='calendar_day verticleMargin'>
-                    <span>${(preLastDay - (firstDayName - 1) + j)}</span><span></span>
-                    </div>`;
-                } else {
-                    calHtml += `
-                    <div class='calendar_day horizonMargin verticleMargin'>
-                    <span>${(preLastDay - (firstDayName - 1) + j)}</span><span></span>
-                    </div>`;
-                }
-            }
-            //1주차 이번달 시작 요일 일때
-            else if (i == 0 && j == firstDayName) {
-                if (j == 0) {
-                    calHtml += `
-                    <div class ='calendar_day horizonMargin verticleMargin'>
-                    <span>${startDayCount}</span>
-                    <span id='${today.getFullYear()}${setFixMonth(today.getMonth())}${setFixDay(startDayCount)}'></span>
-                    </div>`;
-                } else if (j == 6) {
-                    calHtml += `
-                    <div class ='calendar_day verticleMargin'>
-                    <span>${startDayCount}</span>
-                    <span id='${today.getFullYear()}${setFixMonth(today.getMonth())}${setFixDay(startDayCount)}'></span>
-                    </div>`;
-                } else {
-                    calHtml += `
-                    <div class ='calendar_day horizonMargin verticleMargin'>
-                    <span>${startDayCount}</span>
-                    <span id='${today.getFullYear()}${setFixMonth(today.getMonth())}${setFixDay(startDayCount)}'></span>
-                    </div>`;
-                }
-                startDayCount++;
-            }
-            //1주차 이번달 시작 요일 이후일때
-            else if (i == 0 && j > firstDayName) {
-                if (j == 0) {
-                    calHtml += `
-                    <div class ='calendar_day horizonMargin verticleMargin'>
-                    <span>${startDayCount}</span>
-                    <span id='${today.getFullYear()}${setFixMonth(today.getMonth())}${setFixDay(startDayCount)}'></span>
-                    </div>`;
-                } else if (j == 6) {
-                    calHtml += `
-                    <div class ='calendar_day verticleMargin'>
-                    <span>${startDayCount}</span>
-                    <span id='${today.getFullYear()}${setFixMonth(today.getMonth())}${setFixDay(startDayCount)}'></span>
-                    </div>`;
-                } else {
-                    calHtml += `
-                    <div class ='calendar_day horizonMargin verticleMargin'>
-                    <span>${startDayCount}</span>
-                    <span id='${today.getFullYear()}${setFixMonth(today.getMonth())}${setFixDay(startDayCount)}'></span>
-                    </div>`;
-                }
-                startDayCount++;
-            }
-            //이번달 마지막날까지
-            else if (i > 0 && startDayCount <= lastday) {
-                if (j == 0) {
-                    calHtml += `
-                    <div class ='calendar_day horizonMargin verticleMargin'>
-                    <span>${startDayCount}</span>
-                    <span id='${today.getFullYear()}${setFixMonth(today.getMonth())}${setFixDay(startDayCount)}'></span>
-                    </div>`;
-                } else if (j == 6) {
-                    calHtml += `
-                    <div class ='calendar_day verticleMargin'>
-                    <span>${startDayCount}</span>
-                    <span id='${today.getFullYear()}${setFixMonth(today.getMonth())}${setFixDay(startDayCount)}'></span>
-                    </div>`;
-                } else {
-                    calHtml += `
-                    <div class ='calendar_day horizonMargin verticleMargin'>
-                    <span>${startDayCount}</span>
-                    <span id='${today.getFullYear()}${setFixMonth(today.getMonth())}${setFixDay(startDayCount)}'></span>
-                    </div>`;
-                }
-                startDayCount++;
-            }
-            // 이번 달의 마지막 날 이후일 때
-            else if (startDayCount > lastday) {
-                if (j == 0) {
-                    calHtml += `
-                    <div class = 'calendar_day horizonMargin verticleMargin'>
-                    <span>${lastDayCount}</span>
-                    </div>`;
-                } else if (j == 6) {
-                    calHtml += `
-                    <div class = 'calendar_day horizonMargin'>
-                    <span>${lastDayCount}</span>
-                    </div>`;
-                } else {
-                    calHtml += `
-                    <div class = 'calendar_day horizonMargin verticleMargin'>
-                    <span>${lastDayCount}</span>
-                    </div>`;
-                }
-                lastDayCount++;
-            }
-        }
-    }
-}
-
-const setFixDay = (num: number): String => {
-    let fixNum: string = "";
-    if (num <= 10) {
-        fixNum = "0" + (num - 1).toString();
-    } else {
-        fixNum = (num - 1).toString();
-    }
-    return fixNum;
-}
-//id생성하기위함 1~9 ->01 ~ 09
-const setFixMonth = (num: number): String => {
-
-    let fixNum: string = "";
-    if (num + 1 < 10) {
-        fixNum = "0" + (num + 1).toString();
-    } else {
-        fixNum = (num + 1).toString();
-    }
-    return (fixNum);
-}
-*/
 
 export default Calendar;
