62eca7592740f1e8517e61ffc1aa6805f521b169
GYUHA_HWANG <gyuha0310@gmail.com>
2020-10-09 22:49:05 +0900

    package setting

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { Route } from "react-router";
 import { ConnectedRouter } from "connected-react-router";
 import AddHabit from "./components/addHabit/AddHabit";
 import HabitMain from "./components/main/HabitMain";
-import { createMuiTheme, ThemeProvider, Typography } from "@material-ui/core";
 import "./styles/app.css";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 interface AppProps {
   history: History;
@@ -14,20 +14,16 @@ interface AppProps {
 
 const theme = createMuiTheme({
   typography: {
-    fontFamily: "Nanum Pen Script",
   },
 });
-theme.typography.h3 = {
-  fontSize: "1.2rem",
-};
+
 const App = ({ history }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
       <ConnectedRouter history={history}>
-        <Typography variant="h3">sdfsdf</Typography>
-        {/* <Route path="/" component={Header} />
+        <Route path="/" component={Header} />
         <Route path="/" component={HabitMain} exact />
-        <Route path="/add" component={AddHabit} /> */}
+        <Route path="/add" component={AddHabit} />
       </ConnectedRouter>
     </ThemeProvider>
   );
